5f0d694eff60dc472264520d2785cafd51943351
Alistair Lynn <arplynn@gmail.com>
2014-08-22 13:50:21 +0100

    Add a test script

---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+if [ -n "$1" ]; then
+    echo "Testing package $1..."
+    ./polypkg.py "$1"
+    rm -rf components
+else
+    if [ -e components ]; then
+        echo "Aborting, components file exists"
+        exit 1
+    fi
+    PACKAGES=$(grep '^[^ ]' packages.yaml | sed 's/://')
+    for PACKAGE in $PACKAGES
+    do
+        "$0" "$PACKAGE"
+    done
+fi
+

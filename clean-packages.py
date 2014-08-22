84d78bd7bf267607cd7942d2e0cc50d0088bb543
Alistair Lynn <arplynn@gmail.com>
2014-08-22 16:39:39 +0100

    Clean up to use tags

--- clean-packages.py
+++ clean-packages.py
@@ -4,6 +4,10 @@ import yaml
 import os.path
 import urllib.parse
 import re
+import getpass
+import functools
+from hammock import Hammock
+
 from copy import deepcopy
 
 urllib.parse.uses_relative.append('github')
@@ -34,6 +38,40 @@ def strip_github(url):
                              branch='master',
                              path=match.group(3))
 
+shared_github_auth = None
+def github_auth():
+    global shared_github_auth
+    if shared_github_auth is None:
+        username = input('Github username: ')
+        password = getpass.getpass('Github password: ')
+        shared_github_auth = (username, password)
+    return shared_github_auth
+
+GITHUB_API = Hammock('https://api.github.com')
+@functools.lru_cache(maxsize=128)
+def get_latest_release(user, project):
+    endpoint = GITHUB_API.repos(user, project).tags
+    data = endpoint.GET(auth=github_auth()).json()
+    if len(data) == 0:
+        return None
+    return data[0]['name']
+
+def use_latest_release(url):
+    match = re.match(r'https://raw\.githubusercontent\.com/([a-zA-Z0-9_-]+)/([a-zA-Z0-9_-]+)/master/(.*)$', url)
+    if match is None:
+        return url
+    # Get the latest release
+    user = match.group(1)
+    project = match.group(2)
+    path = match.group(3)
+    # use the github API
+    most_recent = get_latest_release(user, project)
+    if most_recent is None:
+        print("WARNING: cannot upgrade to a release for {}/{}, skipping".format(user, project))
+        return url
+    print("Upgrading project {}/{} from master to tag {}".format(user, project, most_recent))
+    return "https://raw.githubusercontent.com/{user}/{project}/{tag}/{path}".format(user=user, project=project, tag=most_recent, path=path)
+
 def clean_package(value):
     backup = deepcopy(value)
     if 'base' in value:
@@ -42,6 +80,8 @@ def clean_package(value):
         value['files'] = {fn: urllib.parse.urljoin(old_base, val) for fn, val in value['files'].items()}
     # Strip the github: URLs
     value['files'] = {fn: strip_github(url) for fn, url in value['files'].items()}
+    # Refuse 'master' from github, use a release
+    value['files'] = {fn: use_latest_release(url) for fn, url in value['files'].items()}
     prefix = os.path.commonprefix(value['files'].values())
     if '/' not in prefix:
         return backup
